5d944f7c57b2fc522faf20e46bb0ae2b21362ee6
Jacky Lam Chay Shean <TP041578@mail.apu.edu.my>
2019-11-12 17:00:37 +0800

    Add new page

--- TestUseofGitHub/Controllers/HomeController.cs
+++ TestUseofGitHub/Controllers/HomeController.cs
@@ -33,6 +33,10 @@ namespace TestUseofGitHub.Controllers
         {
             return View();
         }
+        public IActionResult TestPage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
